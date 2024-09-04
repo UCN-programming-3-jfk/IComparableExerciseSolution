fa48d25548acb25d590681462b8a38ca13bcce44
Jakob Farian Krarup <kr@rup.dk>
2024-09-04 10:24:18 +0200

    Add project files.

---
+++ CardClassLibrary/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CardClassLibrary
+{
+    public enum Suit {Empty, Hearts, Spades, Clubs, Diamonds }
+    public class Card : IComparable, IComparable<Card>
+    {
+        public Suit Suit { get; set; }
+        public int Value { get; set; }
+
+        public Card(int value, Suit suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+
+        public int CompareTo(Card other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if ( obj ! is Card) { throw new ArgumentException("That wasn't a card"); }
+            return CompareTo((Card)obj);    
+        }
+    }
+}
---
+++ CardClassLibrary/HeartilySorter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CardClassLibrary
+{
+    public class HeartilySorter : IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            int xValue = GetValue(x);
+            int yValue = GetValue(y);
+            return xValue.CompareTo(yValue);
+        }
+
+        private int GetValue(Card card)
+        {
+            int value = card.Value;
+            if (card.Suit == Suit.Hearts) { value += 13; }
+            return value;
+        }
+    }
+}
